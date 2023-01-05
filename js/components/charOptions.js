458c68a858367f83c18c0fcc68c903df004e2900
mogamoga <mogamoga.g1thu6@gmail.com>
2023-01-05 18:47:16 +0900

    m

--- js/components/charOptions.js
+++ js/components/charOptions.js
@@ -1,9 +1,34 @@
 
 const CharOptions = {
-    template: ``,
+    template: `
+        <input type="radio" id="option0" value="" v-model="prefix" />
+        <label for="option0">なし</label><br>
+        <input type="radio" id="option1" value=":" v-model="prefix" />
+        <label for="option1">: 印字不可な文字は名称で出力する</label><br>
+        <input type="radio" id="option2" value="@" v-model="prefix" />
+        <label for="option2">@ プレフィックスに#\\をつける</label><br>
+        <input type="radio" id="option3" value=":@" v-model="prefix" />
+        <label for="option3">:@ 印字不可な文字を名称で出力し、Shiftキーについて言及する</label>
+    `,
     emits: ["createdDirective"],
+    data() {
+        return {
+            directiveChar: "c",
+            prefix: ""
+        }
+    },
+    watch: {
+        prefix() {
+            this.creatDirective();
+        }
+    },
     mounted() {
-        this.$emit("createdDirective", "~c");
+        this.creatDirective();
+    },
+    methods: {
+        creatDirective() {
+            this.$emit("createdDirective", `~${this.prefix}${this.directiveChar}`);
+        }
     }
 };
 
