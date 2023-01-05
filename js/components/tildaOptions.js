e384a5ebc330090e3e81ddc7fc22b13e5b383594
mogamoga <mogamoga.g1thu6@gmail.com>
2023-01-05 15:38:11 +0900

    a

--- js/components/tildaOptions.js
+++ js/components/tildaOptions.js
@@ -1,10 +1,8 @@
 
 const TildaOptions = {
     template: `
-        <div>
-          <label for="n">n:繰り返し回数</label>
-          <input type="number" id="n">
-        </div>
+        <label for="n">n:繰り返し回数</label>
+        <input type="number" id="n">
     `
 };
 
