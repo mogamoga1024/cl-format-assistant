4373a5ef980eb6d488074844f5f19a88c5e2cbe3
mogamoga <mogamoga.g1thu6@gmail.com>
2023-01-05 15:48:08 +0900

    a

--- js/components/tildaOptions.js
+++ js/components/tildaOptions.js
@@ -2,7 +2,12 @@
 const TildaOptions = {
     template: `
         <label for="n">n:繰り返し回数</label>
-        <input type="number" id="n">
-    `
+        <input type="number" id="n" v-model="n">
+    `,
+    data() {
+        return {
+            n: 1
+        }
+    },
 };
 
