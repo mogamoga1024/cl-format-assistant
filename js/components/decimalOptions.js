0e8c0d790aa5699b97f89a43a705708d74b68803
mogamoga <mogamoga.g1thu6@gmail.com>
2023-01-05 18:07:54 +0900

    l

--- js/components/decimalOptions.js
+++ js/components/decimalOptions.js
@@ -1,18 +1,39 @@
 
 const DecimalOptions = {
     template: `
-        <label for="n">n:繰り返し回数</label>
-        <input type="number" id="n" v-model="n">
+        <label for="mincol">mincol:最小文字数</label>
+        <input type="number" id="mincol" v-model="mincol"><br>
+        <label for="padchar">padchar:パディングに利用する文字</label>
+        <input type="text" id="padchar" v-model="padchar"><br>
+        <label for="commachar">commachar:カンマに利用する文字</label>
+        <input type="text" id="commachar" v-model="commachar"><br>
+        <label for="comma-interval">comma-interval:カンマの間隔</label>
+        <input type="number" id="comma-interval" v-model="commaInterval">
     `,
     emits: ["createdDirective"],
     data() {
         return {
-            n: 1,
-            defaultN: 1
+            mincol: 1,
+            padchar: " ",
+            commachar: ",",
+            commaInterval: 3,
+            defaultMincol: 1,
+            defaultPadchar: " ",
+            defaultCommachar: ",",
+            defaultCommaInterval: 3
         }
     },
     watch: {
-        n() {
+        mincol() {
+            this.creatDirective();
+        },
+        padchar() {
+            this.creatDirective();
+        },
+        commachar() {
+            this.creatDirective();
+        },
+        commaInterval() {
             this.creatDirective();
         }
     },
@@ -21,10 +42,29 @@ const DecimalOptions = {
     },
     methods: {
         creatDirective() {
+            const needMincol = this.mincol !== this.defaultMincol;
+            const needPadchar = this.padchar !== this.defaultPadchar;
+            const needCommachar = this.commachar !== this.defaultCommachar;
+            const needCommaInterval = this.commaInterval !== this.defaultCommaInterval;
+
+            const mincol = needMincol ? this.mincol : "";
+            const padchar = needPadchar ? this.padchar : "";
+            const commachar = needCommachar ? this.commachar : "";
+
             let directive = `~d`;
-            if (this.n !== this.defaultN) {
-                directive = `~${this.n}d`;
+            if (needMincol && !needPadchar && !needCommachar && !needCommaInterval) {
+                directive = `~${this.mincol}d`;
             }
+            else if (needPadchar && !needCommachar && !needCommaInterval) {
+                directive = `~${mincol},${this.padchar}d`;
+            }
+            else if (needCommachar && !needCommaInterval) {
+                directive = `~${mincol},${padchar},${this.commachar}d`;
+            }
+            else if (needCommaInterval) {
+                directive = `~${mincol},${padchar},${commachar},${this.commaInterval}d`;
+            }
+
             this.$emit("createdDirective", directive);
         }
     }
